8fbc5c9852497beae8a9983fe0e59ccc933bbfc3
M.Fevzi Korkutata <fevzimfk@yahoo.com>
2018-01-29 00:25:15 +0300

    author is added

--- email3rdParty.sh
+++ email3rdParty.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+#Author: M.Fevzi Korkutata | January 2018
+#www.admineer.com/blog
 printf 'Hi,\nWebservice response time is slow and calculated as below.\nPlease check your local system as soon as possible!\nRegards...\n\n'"$1 = $2 seconds" | mail -s "$1 Response time is slow. $(date +"%T")" korkutata@EAMILADDRESS.com
